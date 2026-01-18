c2357ee38dcbe539c71739393b062a6095a24226
Samet Kocabacak <sametkocabacak@hotmail.com>
2026-01-18 21:57:22 +0300

    Delete SpeakerAI-main directory

--- SpeakerAI-main/speakerai/__init__.py
+++
@@ -1,5 +0,0 @@
-"""SpeakerAI package."""
-from .pipeline import SpeakerPipeline
-from .models import ProcessedMeeting, SpeakerProfile, SpeakerTurn
-
-__all__ = ["SpeakerPipeline", "ProcessedMeeting", "SpeakerProfile", "SpeakerTurn"]
